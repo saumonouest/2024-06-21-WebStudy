2d7abe10ec0fb7fcd5b4590ad36bc67b3bf188d8
user <user@DESKTOP-HUGC3B7>
2024-06-27 17:51:53 +0900

    2024/06/27

---
+++ HTMLCSSProject_1/src/main/java/com/sist/dao/MusicVO.java
@@ -0,0 +1,11 @@
+package com.sist.dao;
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+public class MusicVO {
+	private int mno, idcrement;
+	private String title, singer, album, poster, state;
+	
+}
